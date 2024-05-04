308e2bc8863c16d58c658e934dc0ffa4201ab618
Akanimo47 <akanebong0147@gmail.com>
2024-05-05 00:08:33 +0100

    week 2

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Set footer copyright year
+    document.getElementById('copyright-year').textContent = new Date().getFullYear();
+
+    // Set footer last modified date
+    document.getElementById('last-modified').textContent = document.lastModified;
+
+    // Add event listener to the menu button
+    const hamButton = document.getElementById('menu'); // Select the menu button
+    const navigation = document.querySelector('.navigation'); // Select the navigation container
+    hamButton.addEventListener('click', function() {
+        hamButton.classList.toggle('open');
+        navigation.classList.toggle('open');
+    });
+});
